c619517b479400b651296f18e9baf694e7a598d5
SHRIOM <129147531+shriom17@users.noreply.github.com>
2026-01-02 09:59:04 +0530

    Create buySell.java

---
+++ Arrays/buySell.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit=0;
+        int min=prices[0];
+        
+        int mi=0;
+        for(int i=1; i<=prices.length-1; i++)
+        {
+            if(prices[i]<min)
+            {
+                min=prices[i];
+            }
+            profit = Math.max(profit,prices[i]-min);
+        }
+        return profit;
+    }
+}
