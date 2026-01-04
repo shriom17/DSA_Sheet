06911c01449bbf56a0bd4ad36afcb171a3b84378
SHRIOM <129147531+shriom17@users.noreply.github.com>
2026-01-04 13:08:32 +0530

    Create zeroFilledSubArray.java

---
+++ Arrays/zeroFilledSubArray.java
@@ -0,0 +1,20 @@
+class Solution {
+    public long zeroFilledSubarray(int[] nums) {
+        int n=nums.length;
+        long count=0;
+        int seq=0;
+        for(int i=0; i<n; i++)
+        {
+            if(nums[i]==0)
+            {
+                seq++;
+                count += seq;
+            }
+            else
+            {
+                seq=0;
+            }
+        }
+        return count;
+    }
+}
