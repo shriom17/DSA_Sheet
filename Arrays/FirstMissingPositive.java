004e45bb365127829d94258fc8a79016e7e5238e
SHRIOM <129147531+shriom17@users.noreply.github.com>
2026-01-05 09:53:17 +0530

    Update FirstMissingPositive.java

--- Arrays/FirstMissingPositive.java
+++ Arrays/FirstMissingPositive.java
@@ -14,3 +14,30 @@ class Solution {
         return small;
     }
 }
+//Another Approach(Optimal)
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n=nums.length;
+       // int small=1;
+       // Arrays.sort(nums);
+     
+       int temp;
+       for(int i=0; i<n; i++)
+       {
+          while(nums[i]>0 && nums[i]<=n && nums[nums[i]-1]!=nums[i])
+          {
+            temp=nums[nums[i]-1];
+            nums[nums[i]-1]=nums[i];
+            nums[i]=temp;
+          }
+       }
+        for(int i=0;i<n; i++)
+        {
+            if(nums[i]!=i+1)
+            {
+                return i+1;
+            }
+        }
+        return n+1;
+    }
+}
