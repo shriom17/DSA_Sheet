d67adf4a41ecff0084c5f988d0db1b560463e539
SHRIOM <129147531+shriom17@users.noreply.github.com>
2026-01-24 09:47:12 +0530

    Create SubArrayDiv.java

---
+++ PrefixSum/SubArrayDiv.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int subarraysDivByK(int[] nums, int k) {
+        int n = nums.length;
+        int sum=0;
+        int res=0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0,1);
+        for(int i=0; i<n; i++)
+        {
+            sum = sum + nums[i];
+            int rem = sum%k;
+            if(rem<0)
+            {
+                rem += k;
+            }
+            if(map.containsKey(rem))
+            {
+                res += map.get(rem);
+            }
+            map.put(rem, map.getOrDefault(rem,0)+1);
+        }
+        return res;
+    }
+}
