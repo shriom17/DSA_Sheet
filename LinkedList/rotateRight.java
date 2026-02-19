60c83b15d94ba48359e4a089b494fa2680e87ce5
SHRIOM <129147531+shriom17@users.noreply.github.com>
2026-02-19 21:47:28 +0530

    Create rotateRight.java

---
+++ LinkedList/rotateRight.java
@@ -0,0 +1,32 @@
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+
+        if (head == null || head.next == null || k == 0)
+            return head;
+
+        ListNode tail = head;
+        int len = 1;
+
+        while (tail.next != null) {
+            tail = tail.next;
+            len++;
+        }
+
+        k = k % len;
+        if (k == 0) return head;
+
+        tail.next = head;  // circular
+
+        int steps = len - k;
+        ListNode newTail = head;
+
+        for (int i = 1; i < steps; i++) {
+            newTail = newTail.next;
+        }
+
+        ListNode newHead = newTail.next;
+        newTail.next = null;
+
+        return newHead;
+    }
+}
