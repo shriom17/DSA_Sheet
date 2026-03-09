b0941d133e90e791763214e28733dac42c997435
SHRIOM <129147531+shriom17@users.noreply.github.com>
2026-03-09 20:43:29 +0530

    Fix formatting of while loop in expand method

--- Two Pointers/longestPalindrome.java
+++ Two Pointers/longestPalindrome.java
@@ -22,7 +22,8 @@ class Solution {
 
     public int expand(String s, int left, int right) {
 
-        while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+        while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right))
+        {
             left--;
             right++;
         }
