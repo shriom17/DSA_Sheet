92531c03906defe00ca14c37d69f1f063daf9dde
SHRIOM <129147531+shriom17@users.noreply.github.com>
2026-03-15 11:18:37 +0530

    Update find132pattern.java

--- Stack/find132pattern.java
+++ Stack/find132pattern.java
@@ -16,3 +16,29 @@ class Solution {
         return false;
     }
 }
+
+// Another Method
+import java.util.Stack;
+
+class Solution {
+    public boolean find132pattern(int[] nums) {
+        int n = nums.length;
+        Stack<Integer> st = new Stack<>();
+        int second = Integer.MIN_VALUE;   // nums[k]
+
+        for(int i = n-1; i >= 0; i--) {
+
+            if(nums[i] < second) {
+                return true;
+            }
+
+            while(!st.isEmpty() && nums[i] > st.peek()) {
+                second = st.pop();
+            }
+
+            st.push(nums[i]);
+        }
+
+        return false;
+    }
+}
