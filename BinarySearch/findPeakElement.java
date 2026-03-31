aeb0ed5f51e5f58834e178d4bc271ca621d60518
SHRIOM <129147531+shriom17@users.noreply.github.com>
2026-03-31 11:12:47 +0530

    Update findPeakElement.java

--- BinarySearch/findPeakElement.java
+++ BinarySearch/findPeakElement.java
@@ -8,3 +8,20 @@ class Solution {
         return nums.length - 1;
     }
 }
+//Binary Search logic
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int l = 0, r = nums.length - 1;
+
+        while(l < r) {
+            int mid = (l + r) / 2;
+
+            if(nums[mid] > nums[mid + 1]) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return l;
+    }
+}
