19fa25536cd60bcb4d79d1b33dced20141c0c22a
SHRIOM <129147531+shriom17@users.noreply.github.com>
2026-04-10 08:53:37 +0530

    Create preorderTraversal.java

---
+++ Trees/preorderTraversal.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList <>();
+        bfs(root, res);
+        return res;
+    }
+    private void bfs (TreeNode node, List<Integer>res)
+    {
+        if(node==null)
+        {
+            return;
+        }
+        res.add(node.val);
+        bfs(node.left,res);
+        bfs(node.right,res);
+    }
+}
