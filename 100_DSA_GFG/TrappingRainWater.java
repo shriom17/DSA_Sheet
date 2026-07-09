29d5ab3acae62be50964be48484568955a630f92
SHRIOM <129147531+shriom17@users.noreply.github.com>
2026-07-09 09:01:51 +0530

    Update TrappingRainWater.java

--- 100_DSA_GFG/TrappingRainWater.java
+++ 100_DSA_GFG/TrappingRainWater.java
@@ -24,3 +24,31 @@ class Solution {
       return res;
     }
 }
+class Solution {
+    public int maxWater(int arr[]) {
+        // code here
+      int n = arr.length;
+      int res = 0;
+      int[] leftmax = new int[n];
+      int[ ] rightmax = new int[n];
+      leftmax[0]=arr[0];
+      rightmax[n-1]=arr[n-1];
+      
+      for(int i=1; i<n; i++)
+      {
+          leftmax[i] = Math.max(leftmax[i-1],arr[i]);
+      }
+      
+      for(int i=n-2; i>=0; i--)
+      {
+          rightmax[i] = Math.max(rightmax[i+1],arr[i]);
+      }
+      
+      for(int i=0; i<n; i++)
+      {
+          res = res + Math.min(leftmax[i], rightmax[i]) - arr[i];
+      }
+     
+      return res;
+    }
+}
