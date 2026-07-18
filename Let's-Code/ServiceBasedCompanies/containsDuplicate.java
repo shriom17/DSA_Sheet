58ce70381f28fd76d7e6cd00faffa04f6ce63352
SHRIOM <129147531+shriom17@users.noreply.github.com>
2026-07-18 09:03:47 +0530

    Create containsDuplicate.java

---
+++ Let's-Code/ServiceBasedCompanies/containsDuplicate.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        boolean flag = false;
+        HashSet<Integer> set = new HashSet<>();
+        
+        for(int i=0; i<nums.length; i++)
+        {
+            if(!set.add(nums[i]))
+            {
+                flag = true;
+            }
+
+        }
+
+        return flag;
+    }
+}
