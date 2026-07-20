b5618fe72cf819ffd4b5a99afac3319b66927778
SHRIOM <129147531+shriom17@users.noreply.github.com>
2026-07-20 12:32:46 +0530

    Create peakElement.java

---
+++ 100_DSA_GFG/Searching&Sorting/peakElement.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int peakElement(int[] arr) {
+        // code here
+        int n=arr.length;
+        int left=0;
+        int right=n-1;
+        
+        while(left<right)
+        {
+            int mid=left+(right-left)/2;
+            if(arr[mid]<arr[mid+1])
+            {
+                left=mid+1;
+            }
+            else
+            {
+                right=mid;
+            }
+        }
+        
+        return left;
+    }
+}
