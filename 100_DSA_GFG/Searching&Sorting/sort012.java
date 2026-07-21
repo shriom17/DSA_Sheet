cd0afb68fa7cd7470ea165aee88f33b59e4c7987
SHRIOM <129147531+shriom17@users.noreply.github.com>
2026-07-21 09:49:04 +0530

    Update sort012.java

--- 100_DSA_GFG/Searching&Sorting/sort012.java
+++ 100_DSA_GFG/Searching&Sorting/sort012.java
@@ -4,3 +4,38 @@ class Solution {
         Arrays.sort(arr);
     }
 }
+//Optimized
+class Solution {
+    public void sort012(int[] arr) {
+        // code here
+        //Arrays.sort(arr);
+        int n=arr.length;
+        int left=0;
+        int right=n-1;
+        int mid=0;
+        
+        while(mid<=right)
+        {
+            if(arr[mid]==2)
+            {
+                int a=arr[mid];
+                arr[mid]=arr[right];
+                arr[right]=a;
+                right--;
+            }
+            else if(arr[mid]==0)
+            {
+                int a=arr[mid];
+                arr[mid]=arr[left];
+                arr[left]=a;
+                left++;
+                mid++;
+            }
+            else
+            {
+                mid++;
+            }
+        }
+        
+    }
+}
