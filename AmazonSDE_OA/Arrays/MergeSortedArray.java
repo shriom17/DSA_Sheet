a3f691d85e83e2a5519eeb514dddc71e89a5582b
SHRIOM <129147531+shriom17@users.noreply.github.com>
2026-08-03 00:00:08 +0530

    Create MergeSortedArray.java

---
+++ AmazonSDE_OA/Arrays/MergeSortedArray.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+        int i = m - 1;   // nums1 pointer
+        int j = n - 1;   // nums2 pointer
+        int k = m + n - 1; // last index
+
+        while (j >= 0) {
+            if (i >= 0 && nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
+        }
+    }
+}
