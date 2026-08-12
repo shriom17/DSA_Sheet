b432de8ff61d5a15d0e9854023ba98aec5ebb1eb
SHRIOM <129147531+shriom17@users.noreply.github.com>
2026-08-12 08:31:20 +0530

    Add subarrayXor method in Solution class
    Implement subarrayXor method to count subarrays with given XOR.

---
+++ 100_DSA_GFG/Hashing/subarrayXor.java
@@ -0,0 +1,21 @@
+class Solution {
+    public long subarrayXor(int arr[], int k) {
+        // code here
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int xor=0;
+        long ans=0;
+        map.put(0,1);
+        for(int i=0; i<arr.length; i++)
+        {
+            xor=xor^arr[i];
+            if(map.containsKey(xor^k))
+            {
+                ans+=map.get(xor^k);
+            }
+            
+            map.put(xor, map.getOrDefault(xor,0)+1);
+        }
+        
+        return ans;
+    }
+}
