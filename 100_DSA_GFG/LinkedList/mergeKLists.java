10b0b1d8fd49d5820518aabe2e7afd3c2f855022
SHRIOM <129147531+shriom17@users.noreply.github.com>
2026-08-26 23:29:04 +0530

    Update mergeKLists.java

--- 100_DSA_GFG/LinkedList/mergeKLists.java
+++ 100_DSA_GFG/LinkedList/mergeKLists.java
@@ -18,32 +18,30 @@ class Solution {
         Node head = null;
         Node tail = null;
 
-        // সব linked list-এর head PriorityQueue-তে রাখি
+
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] != null) {
                 minPq.add(arr[i]);
             }
         }
 
-        // যতক্ষণ queue-তে node আছে
+        
         while (!minPq.isEmpty()) {
 
-            // সবচেয়ে ছোট node বের করি
+            
             Node current = minPq.poll();
 
-            // প্রথম node হলে
+           ে
             if (head == null) {
                 head = current;
                 tail = current;
             }
             else {
-                // merged list-এর শেষে current যোগ করি
+                ি
                 tail.next = current;
                 tail = current;
             }
 
-            // current যে list থেকে এসেছে,
-            // সেই list-এর পরের node queue-তে ঢোকাই
             if (current.next != null) {
                 minPq.add(current.next);
             }
