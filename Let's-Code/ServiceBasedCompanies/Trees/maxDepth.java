e1edc3a77cab5869ccd451bb34b22948e6fc1a8b
SHRIOM <129147531+shriom17@users.noreply.github.com>
2026-08-27 10:50:57 +0530

    Create maxDepth.java

---
+++ Let's-Code/ServiceBasedCompanies/Trees/maxDepth.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+       return count(root);
+    }
+
+    private int count(TreeNode r)
+    {
+         if(r == null)
+        {
+            return 0;
+        }
+
+        int left = count(r.left);
+        int right = count(r.right);
+
+        return 1 + Math.max(left, right);
+    }
+}
