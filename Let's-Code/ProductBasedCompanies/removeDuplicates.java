64352b948c7077809099f6479b2fa4f71fb2bca1
SHRIOM <129147531+shriom17@users.noreply.github.com>
2026-09-24 21:44:31 +0530

    Create removeDuplicates.java

---
+++ Let's-Code/ProductBasedCompanies/removeDuplicates.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int removeDuplicates(int[] nums) 
+    {
+        int pos=1;
+        for(int i=1; i<=nums.length-1; i++)
+        {
+            if(nums[i]!=nums[pos-1])
+            {
+                nums[pos]=nums[i];
+                pos++;
+            }
+        }
+        return pos;
+    }
+}
